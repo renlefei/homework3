8a7c8fe6a65a5af59a2fcc9bd0641014d73f805e
renlefei <renlefei16@163.com>
2020-06-03 09:57:53 +0800

    fix naming issue

--- src/test/java/parking/lot/ParkingBoyTest.java
+++ src/test/java/parking/lot/ParkingBoyTest.java
@@ -7,12 +7,13 @@ public class ParkingBoyTest {
     @Test
     public void ShouldParkCatToFirstParkingLotWithSpace(){
         ParkingBoy parkingBoy = new ParkingJuniorBoy("PJ");
-        ParkingLot parkingLot1 = new ParkingLot("parkingLot1", 1);
-        ParkingLot parkingLot2 = new ParkingLot("parkingLot1", 1);
+        ParkingLot parkingLot1 = new ParkingLot("parkingLot1", 2);
+        ParkingLot parkingLot2 = new ParkingLot("parkingLot2", 1);
 
         parkingBoy.Add(parkingLot1);
         parkingBoy.Add(parkingLot2);
 
+        assert parkingLot1 == parkingBoy.ParkCar(new Car());
         assert parkingLot1 == parkingBoy.ParkCar(new Car());
         assert parkingLot2 == parkingBoy.ParkCar(new Car());
     }
